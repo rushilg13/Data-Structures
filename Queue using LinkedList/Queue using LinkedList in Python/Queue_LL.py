31950bded646189172dd2f29da8b832921d4c749
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-26 23:04:27 +0530

    created Enqueue

--- Queue using LinkedList/Queue using LinkedList in Python/Queue_LL.py
+++ Queue using LinkedList/Queue using LinkedList in Python/Queue_LL.py
@@ -3,6 +3,13 @@ class Node:
         self.data = data
         self.next = None
 
-
+class Queue_LL:
+    def __init__(self):
+        self.start = None
+    
+    def enqueue(self, value):
+        newNode = Node(value)
+        newNode.next = self.start.next
+        self.start = newNode
     
     
