0fe48d9ea12cf2980172e1b1b1c9f2393f32f533
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-26 22:00:47 +0530

    Added Pop Method

--- Stack using LinkedList/Using Python/Stack_LL.py
+++ Stack using LinkedList/Using Python/Stack_LL.py
@@ -7,7 +7,7 @@ class Stack_LL:
     def __init__(self):
         self.start = None
 
-    def insert(self, value):
+    def push(self, value):
         if self.start == None:
             self.start = Node(value)   
         else:
@@ -24,11 +24,23 @@ class Stack_LL:
             while(temp!=None):
                 print(temp.data, end="->")
                 temp = temp.next
-            print("Top")    
+            print("Top")
+
+    def pop(self):
+        if self.start == None:
+            print("Stack is Empty!")
+        else:
+            temp = self.start
+            while(temp.next.next!=None):
+                temp = temp.next
+            temp.next = None     
 
 
 SLL = Stack_LL()
-SLL.insert(10)
-SLL.insert(20)
-SLL.insert(30)     
+SLL.push(10)
+SLL.push(20)
+SLL.push(30)
+SLL.push(40)     
+SLL.display()
+SLL.pop()
 SLL.display()
