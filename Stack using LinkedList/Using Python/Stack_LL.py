e924417128556329e465fdbc4d1b18fb69d0d614
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-26 20:46:43 +0530

    Stack_LL

---
+++ Stack using LinkedList/Using Python/Stack_LL.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Stack_LL:
+    def __init__(self):
+        self.start = None
+    def insert(self, value):
+        pass
