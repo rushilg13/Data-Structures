f8a0f523e14d7521885fd5731149444f6d2ecf6c
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-26 21:56:59 +0530

    Added Display Method

--- Stack using LinkedList/Using Python/Stack_LL.py
+++ Stack using LinkedList/Using Python/Stack_LL.py
@@ -16,9 +16,19 @@ class Stack_LL:
                 temp = temp.next
             temp.next = Node(value)
     
+    def display(self):
+        if self.start==None:
+            print("Stack is Empty!")
+        else:
+            temp = self.start
+            while(temp!=None):
+                print(temp.data, end="->")
+                temp = temp.next
+            print("Top")    
 
 
 SLL = Stack_LL()
 SLL.insert(10)
 SLL.insert(20)
 SLL.insert(30)     
+SLL.display()
