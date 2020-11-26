c2aa450cee06ab47f1fd3b9216295560cd8dbe11
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-27 02:27:37 +0530

    Created object and made a bin tree

--- Heaps/Heap using Python/Heap.py
+++ Heaps/Heap using Python/Heap.py
@@ -7,3 +7,24 @@ class Node:
 class Heap:
     def __init__(self, value):
         self.root = Node(value)
+    
+
+
+heap = Heap(100)
+heap.root.left = Node(40)
+heap.root.left.left = Node(30)
+heap.root.left.right = Node(20)
+heap.root.right = Node(50)
+heap.root.right.left = Node(40)
+heap.root.right.right = Node(30)
+
+'''
+            100
+           /    \
+          40     50
+        /   \   /  \
+       30   20 40   30     
+
+'''
+
+
