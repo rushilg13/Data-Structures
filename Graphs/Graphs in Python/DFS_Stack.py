09a10bb2dac409e14e01e965f79e1d474c8b7478
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-28 05:16:49 +0530

    Added DFS

--- Graphs/Graphs in Python/DFS_Stack.py
+++ Graphs/Graphs in Python/DFS_Stack.py
@@ -14,21 +14,50 @@ class Stack:
     def pop(self):
         return self.items[-1]
     
-    def size(size):
+    def size(self):
         return len(self.items)
     
 class DepthFirstSearch:
     def DFS(self, start):
         stack = Stack()
         stack.push(start)
-        print(start)
+        print(start.name)
         start.visited = True
-
-        while stack.size() > 0:
+        while stack:
             node = stack.pop()
-            if len(node.adjacent) > 0:
-                for adj_node in node.adjacent:
+            for adj_node in node.adjacent:
+                if adj_node.visited==False:
                     stack.push(adj_node)
                     adj_node.visited = True
 
 
+Node1 = Node("A")
+Node2 = Node("B")
+Node3 = Node("C")
+Node4 = Node("D")
+Node5 = Node("E")
+Node6 = Node("F")
+Node7 = Node("G")
+Node8 = Node("H")
+
+Node1.adjacent.append(Node2)
+Node1.adjacent.append(Node6)
+Node1.adjacent.append(Node7)
+Node2.adjacent.append(Node3)
+Node2.adjacent.append(Node4)
+Node7.adjacent.append(Node8)
+Node4.adjacent.append(Node5)
+
+'''
+                          A
+                      /   |  \   
+                      B   F   G
+                    /  \       \
+                   C    D        H
+                       /
+                      E
+'''
+
+depthfirstsearch = DepthFirstSearch()
+dfs = depthfirstsearch.DFS(Node1)
+# OUTPUT 
