4900683b5d639b9ba9b277166d269bf5710be4a7
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-28 04:40:15 +0530

    Added Class stack

--- Graphs/Graphs in Python/DFS_Stack.py
+++ Graphs/Graphs in Python/DFS_Stack.py
@@ -3,3 +3,17 @@ class Node:
         self.name = name
         self.adjacent = []
         self.visited = False
+
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        return self.items[-1]
+    
+    def size(size):
+        return len(self.items)
+    
