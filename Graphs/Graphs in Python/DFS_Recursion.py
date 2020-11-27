5e1716d3add86eda6245e85a62888701949d1a0c
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-28 04:10:00 +0530

    Added DSF

--- Graphs/Graphs in Python/DFS_Recursion.py
+++ Graphs/Graphs in Python/DFS_Recursion.py
@@ -4,3 +4,11 @@ class Node:
         self.adjacent = []
         self.visited = False
 
+class DepthFirstSearch:
+    def DFS(self, start):
+        start.visited = True
+        print(start.name)
+
+        for adj_node in start.adjacent:
+            if adj_node.visited == False:
+                DFS(adj_node)
