3716f18fadc89f807bafa586de41fd6535d93279
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-28 03:41:44 +0530

    Created class BFS

--- Graphs/Graphs in Python/BFS.py
+++ Graphs/Graphs in Python/BFS.py
@@ -13,4 +13,26 @@ class Queue:
     
     def dequeue(self):
         return self.items.pop()
+
+class BreadthFirstSearch:
+    def BFS(self, start):
+        queue = Queue()
+        queue.enqueue(start)
+        start.visited = True
+
+        while len(queue)>0:
+            node = queue.dequeue()
+            print(node)
+            if len(node.adjacent) > 0:
+                for adj_node in node.adjacent:
+                    if adj_node is False:
+                        queue.enqueue(adj_node)
+                        adj_node.visited = True
+
+
+
+
+
+
+
     
