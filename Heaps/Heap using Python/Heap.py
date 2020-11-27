0e856b127d3f5e55ab4694210901837432a36a54
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-28 01:44:20 +0530

    Added Level Order Traversal

--- Heaps/Heap using Python/Heap.py
+++ Heaps/Heap using Python/Heap.py
@@ -22,7 +22,7 @@ class Queue:
             return self.items.pop()
     
     def peek(self):
-        return self.items[-1].value
+        return self.items[-1].data
     
     def size(self):
         return len(self.items)
@@ -32,7 +32,33 @@ class Heap:
         self.root = Node(value)
     
     def Levelorder(self, start, traversal):
-        pass
+        if start == None:
+            return
+        queue = Queue()
+        arr=[]
+        queue.enqueue(start)
+        traversal = ""
+        while queue.size() > 0:
+            traversal+=str(queue.peek()) + "->"
+            arr.append(queue.peek())
+            node = queue.dequeue()
+            if node.left:
+                queue.enqueue(node.left)
+            if node.right:
+                queue.enqueue(node.right)
+        print(traversal, end="End") 
+    
+    # def check_heap(self):
+    #     arr = self.Levelorder(heap.root, "")
+    #     arr.insert(0,None)
+    #     for i in range(1,len(arr)):
+    #         if(arr[int(i/2)] > arr[i*2] and arr[int(i/2)] > arr[(i*2)+1]):
+    #             print("heap")
+    #         else:
+    #             print("no")
+    #     print(arr)
+
+
 
     
 
@@ -44,7 +70,7 @@ heap.root.left.right = Node(20)
 heap.root.right = Node(50)
 heap.root.right.left = Node(40)
 heap.root.right.right = Node(30)
-
+heap.Levelorder(heap.root, "")
 '''
             100
            /    \
