dc480eadb8f3325ed7a14998298a0ba083cd4507
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-29 03:17:49 +0530

    Added Insert method

--- Linked List/Linked List in Python/Sum_LLs.py
+++ Linked List/Linked List in Python/Sum_LLs.py
@@ -3,3 +3,26 @@ class Node:
         self.value = value
         self.next = None
 
+class LinkedList:
+    def __init__(self):
+        self.start = None
+    
+    def insert(self, value):
+        if self.start == None:
+            self.start = Node(value)
+        else:
+            temp = self.start
+            while temp.next != None:
+                temp = temp.next
+            temp.next = Node(value)
+    
+    def display(self):
+        if self.start == None:
+            print("List Empty!")
+        else:
+            temp = self.start
+            while temp != None:
+                print(temp.value, end = "->")
+                temp = temp.next
+            print("End")
+    
