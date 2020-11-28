b6947e8c5c0dcc93fa5019210dd9091cdc4c6753
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-28 18:26:04 +0530

    Added Count

--- Hash Tables/Hash Tables in Python/Hash_Table.py
+++ Hash Tables/Hash Tables in Python/Hash_Table.py
@@ -1,6 +1,6 @@
 class HashTable:
     def __init__(self):
-        self.MAX = 100
+        self.MAX = 10
         self.arr = [None for i in range(self.MAX)]
     
     def print_arr(self):
@@ -13,8 +13,28 @@ class HashTable:
     def add(self, key, value):
         ind = self.get_key(key)
         self.arr[ind] = value 
+    
+    def count(self):
+        count = 0
+        for i in self.arr:
+            if(i!=None):
+                count+=1
+        print("Currently there are", count, "entries in Hash table")
+
 
 
 hash = HashTable()
-hash.add(21,"Austin")
+hash.add(617,"Austin")
+hash.add(411,"Moon")
+hash.add(132,"Gerald")
+hash.add(243,"Mathew")
+hash.add(218,"Andrew")
+hash.add(959,"Kylie")
+hash.add(0,"Timberlake")
+hash.add(624,"Justin")
+hash.add(95,"Sarah")
+hash.add(916,"Miley")
+
 hash.print_arr()
+
+hash.count()
