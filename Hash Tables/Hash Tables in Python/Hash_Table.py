e5511a33c08958476b81c90eb88da1625ae5ddce
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-28 18:17:41 +0530

    Added add Method

--- Hash Tables/Hash Tables in Python/Hash_Table.py
+++ Hash Tables/Hash Tables in Python/Hash_Table.py
@@ -8,8 +8,13 @@ class HashTable:
     
     def get_key(self, value):
         key = value % self.MAX
-        print(key)
+        return key
+    
+    def add(self, key, value):
+        ind = self.get_key(key)
+        self.arr[ind] = value 
 
 
 hash = HashTable()
+hash.add(21,"Austin")
 hash.print_arr()
