ea942a435fd8c717a6ffc77a89b00c43faec8c40
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-28 18:03:55 +0530

    added new Methods

--- Hash Tables/Hash Tables in Python/Hash_Table.py
+++ Hash Tables/Hash Tables in Python/Hash_Table.py
@@ -2,6 +2,14 @@ class HashTable:
     def __init__(self):
         self.MAX = 100
         self.arr = [None for i in range(self.MAX)]
+    
+    def print_arr(self):
+        print(self.arr)
+    
+    def get_key(self, value):
+        key = value % self.MAX
+        print(key)
 
 
 hash = HashTable()
+hash.print_arr()
