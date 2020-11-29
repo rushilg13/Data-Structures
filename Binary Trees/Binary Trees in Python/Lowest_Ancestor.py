52278df45205009413c9345e6dca1a22f7631046
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-29 16:12:06 +0530

    added to stack

--- Binary Trees/Binary Trees in Python/Lowest_Ancestor.py
+++ Binary Trees/Binary Trees in Python/Lowest_Ancestor.py
@@ -13,6 +13,9 @@ class Stack:
     
     def pop(self):
         self.items.pop(-1)
+    
+    def peek(self):
+        return self.items[-1]
 
 class binary:
     def __init__(self, value):
@@ -23,7 +26,22 @@ class binary:
             return
         if node1==self.root or node2 == self.root:
             print(self.root.value)
-        if             
+        else:
+            stack1 = Stack()
+            stack2 = Stack()
+            temp1 = self.root
+            # stack1.push(self.root)
+            # stack2.push(self.root)
+            while temp1 != node1:
+                stack1.push(temp1)
+                temp1 = temp1.left
+            temp1 = self.root
+            while temp1 != node2:
+                stack2.push(temp1)
+                temp1 = temp1.left
+            
+
+                      
 
 tree = binary(1)
 tree.root.left = Node(2)
