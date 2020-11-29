e66f8727bac9d2561f46ce305ad561c8d306f6c9
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-29 15:25:34 +0530

    Added Classes

---
+++ Binary Trees/Binary Trees in Python/Lowest_Ancestor.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class binary:
+    def __init__(self, value):
+        self.root = Node(value)
