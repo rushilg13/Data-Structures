23b14e1ae8fba188bfe4f3ea2b2598b014cbb7d4
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-30 18:35:36 +0530

    added class Node

---
+++ Binary Trees/Binary Trees in Python/LCA_BST.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self, value):
+        self.root = Node(value)
+
