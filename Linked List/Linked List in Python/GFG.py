4ecf56195d66133eafb71a22c8ad164194e0e267
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-12-03 20:30:18 +0530

    Added class stack

--- Linked List/Linked List in Python/GFG.py
+++ Linked List/Linked List in Python/GFG.py
@@ -1,92 +1,90 @@
-# https://www.geeksforgeeks.org/write-a-c-function-to-print-the-middle-of-the-linked-list/
+# # https://www.geeksforgeeks.org/write-a-c-function-to-print-the-middle-of-the-linked-list/
 
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+# class Node:
+#     def __init__(self, value):
+#         self.value = value
+#         self.next = None
 
-class LinkedList:
-    def __init__(self):
-        self.start = None
+# class LinkedList:
+#     def __init__(self):
+#         self.start = None
 
-    def insert(self, value):
-        if self.start == None:
-            self.start = Node(value)
-        else:
-            temp = self.start
-            while(temp.next != None):
-                temp = temp.next
-            temp.next = Node(value)
+#     def insert(self, value):
+#         if self.start == None:
+#             self.start = Node(value)
+#         else:
+#             temp = self.start
+#             while(temp.next != None):
+#                 temp = temp.next
+#             temp.next = Node(value)
     
-    def display(self):
-        if self.start == None:
-            print("LinkedList is Empty!")
-        else:
-            temp = self.start
-            while(temp != None):
-                print(temp.value, end="->")
-                temp = temp.next
-            print("End")
+#     def display(self):
+#         if self.start == None:
+#             print("LinkedList is Empty!")
+#         else:
+#             temp = self.start
+#             while(temp != None):
+#                 print(temp.value, end="->")
+#                 temp = temp.next
+#             print("End")
     
-    # APPROACH 1, Count size, then go halfway
-    def size(self):
-        count = 0
-        if self.start == None:
-            print("LinkedList is Empty!")
-            return 0
-        else:
-            temp = self.start
-            while(temp != None):
-                count+=1
-                temp = temp.next
-            return count
+#     # APPROACH 1, Count size, then go halfway
+#     def size(self):
+#         count = 0
+#         if self.start == None:
+#             print("LinkedList is Empty!")
+#             return 0
+#         else:
+#             temp = self.start
+#             while(temp != None):
+#                 count+=1
+#                 temp = temp.next
+#             return count
     
-    def Middle_1(self):
-        count = LL.size()
-        if count == 0:
-            print("No elements in list!")
-        if count%2 == 0:
-            counter = 0
-            temp = self.start
-            while(temp != None and counter != (count/2)-1):
-                counter+=1
-                temp = temp.next
-            print("Middle Value is",temp.value)
+#     def Middle_1(self):
+#         count = LL.size()
+#         if count == 0:
+#             print("No elements in list!")
+#         if count%2 == 0:
+#             counter = 0
+#             temp = self.start
+#             while(temp != None and counter != (count/2)-1):
+#                 counter+=1
+#                 temp = temp.next
+#             print("Middle Value is",temp.value)
 
-        if count%2 != 0:
-            counter = 0
-            temp = self.start
-            while(temp != None and counter != int(count/2)):
-                counter+=1
-                temp = temp.next
-            print("Middle Value is", temp.value)
+#         if count%2 != 0:
+#             counter = 0
+#             temp = self.start
+#             while(temp != None and counter != int(count/2)):
+#                 counter+=1
+#                 temp = temp.next
+#             print("Middle Value is", temp.value)
     
-    # APPROACH 2, Traverse linked list using two pointers. Move one pointer by one and the other pointers by two. When the fast pointer reaches the end slow pointer will reach the middle of the linked list.
+#     # APPROACH 2, Traverse linked list using two pointers. Move one pointer by one and the other pointers by two. When the fast pointer reaches the end slow pointer will reach the middle of the linked list.
 
-    def Middle_2(self):
-        if self.start == None:
-            print("LinkedList is Empty!")
-        else:
-            behind = self.start
-            ahead = self.start
-            while(ahead.next != None):
-                behind = behind.next
-                ahead = ahead.next.next
-            print("Middle Value is", behind.value)  
+#     def Middle_2(self):
+#         if self.start == None:
+#             print("LinkedList is Empty!")
+#         else:
+#             behind = self.start
+#             ahead = self.start
+#             while(ahead.next != None):
+#                 behind = behind.next
+#                 ahead = ahead.next.next
+#             print("Middle Value is", behind.value)  
 
-LL = LinkedList()
-LL.display()
-LL.insert(10)
-LL.insert(20)
-LL.insert(30)
-LL.insert(40)
-LL.insert(50)
-LL.display()
-print(LL.size())
-LL.Middle_1()
-LL.Middle_2()
-
-
+# LL = LinkedList()
+# LL.display()
+# LL.insert(10)
+# LL.insert(20)
+# LL.insert(30)
+# LL.insert(40)
+# LL.insert(50)
+# LL.display()
+# print(LL.size())
+# LL.Middle_1()
+# LL.Middle_2()
 
 
 
@@ -97,79 +95,141 @@ LL.Middle_2()
 
 
 
-# https://practice.geeksforgeeks.org/problems/delete-middle-of-linked-list/1
 
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
 
-class LinkedList:
-    def __init__(self):
-        self.start = None
+# # https://practice.geeksforgeeks.org/problems/delete-middle-of-linked-list/1
 
-    def insert(self, value):
-        if self.start == None:
-            self.start = Node(value)
-        else:
-            temp = self.start
-            while(temp.next != None):
-                temp = temp.next
-            temp.next = Node(value)
+# class Node:
+#     def __init__(self, value):
+#         self.value = value
+#         self.next = None
+
+# class LinkedList:
+#     def __init__(self):
+#         self.start = None
+
+#     def insert(self, value):
+#         if self.start == None:
+#             self.start = Node(value)
+#         else:
+#             temp = self.start
+#             while(temp.next != None):
+#                 temp = temp.next
+#             temp.next = Node(value)
     
-    def display(self):
-        if self.start == None:
-            print("LinkedList is Empty!")
-        else:
-            temp = self.start
-            while(temp != None):
-                print(temp.value, end="->")
-                temp = temp.next
-            print("End")
+#     def display(self):
+#         if self.start == None:
+#             print("LinkedList is Empty!")
+#         else:
+#             temp = self.start
+#             while(temp != None):
+#                 print(temp.value, end="->")
+#                 temp = temp.next
+#             print("End")
     
-    # APPROACH 1, Count size, then go halfway
-    def size(self):
-        count = 0
-        if self.start == None:
-            print("LinkedList is Empty!")
-            return 0
-        else:
-            temp = self.start
-            while(temp != None):
-                count+=1
-                temp = temp.next
-            return count
+#     # APPROACH 1, Count size, then go halfway
+#     def size(self):
+#         count = 0
+#         if self.start == None:
+#             print("LinkedList is Empty!")
+#             return 0
+#         else:
+#             temp = self.start
+#             while(temp != None):
+#                 count+=1
+#                 temp = temp.next
+#             return count
     
-    def Middle_1(self):
-        count = LL.size()
-        if count == 0:
-            print("No elements in list!")
-        if count%2 == 0:
-            counter = 0
-            temp = self.start
-            while(temp.next != None and counter != (count/2)-1):
-                counter+=1
-                temp = temp.next
-            print("Middle Value is",temp.value)
-            temp.next = temp.next.next
+#     def Middle_1(self):
+#         count = LL.size()
+#         if count == 0:
+#             print("No elements in list!")
+#         if count%2 == 0:
+#             counter = 0
+#             temp = self.start
+#             while(temp.next != None and counter != (count/2)-1):
+#                 counter+=1
+#                 temp = temp.next
+#             print("Middle Value is",temp.value)
+#             temp.next = temp.next.next
         
 
-        if count%2 != 0:
-            counter = 0
-            temp = self.start
-            while(temp.next != None and counter != int(count/2)):
-                counter+=1
-                temp = temp.next
-            print("Middle Value is", temp.value)
-            temp.next = temp.next.next
+#         if count%2 != 0:
+#             counter = 0
+#             temp = self.start
+#             while(temp.next != None and counter != int(count/2)):
+#                 counter+=1
+#                 temp = temp.next
+#             print("Middle Value is", temp.value)
+#             temp.next = temp.next.next
      
 
-LL = LinkedList()
-LL.display()
-LL.insert(10)
-LL.insert(20)
-LL.insert(30)
-LL.insert(40)
-LL.display()
-print(LL.size())
-LL.Middle_1()
+# LL = LinkedList()
+# LL.display()
+# LL.insert(10)
+# LL.insert(20)
+# LL.insert(30)
+# LL.insert(40)
+# LL.display()
+# print(LL.size())
+# LL.Middle_1()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# https://www.careercup.com/question?id=5717797377146880
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        self.items.pop()
+    
+    def sum_all(self):
+        sum = 0
+        for i in self.items:
+            sum += i
+        return sum
+
+class LinkedList:
+    def __init__(self):
+        self.start = None
+
+    def insert(self, value):
+        if self.start == None:
+            self.start = Node(value)
+        else:
+            temp = self.start
+            while(temp.next != None):
+                temp = temp.next
+            temp.next = Node(value)
+    
+    def display(self):
+        if self.start == None:
+            print("LinkedList is Empty!")
+        else:
+            temp = self.start
+            while(temp != None):
+                print(temp.value, end="->")
+                temp = temp.next
+            print("End")
