7cc73b4cccbc32edeed84fe2ecd261b8066ec213
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-12-03 20:18:56 +0530

    Added Problem

--- Linked List/Linked List in Python/GFG.py
+++ Linked List/Linked List in Python/GFG.py
@@ -86,3 +86,90 @@ print(LL.size())
 LL.Middle_1()
 LL.Middle_2()
 
+
+
+
+
+
+
+
+
+
+
+
+# https://practice.geeksforgeeks.org/problems/delete-middle-of-linked-list/1
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.start = None
+
+    def insert(self, value):
+        if self.start == None:
+            self.start = Node(value)
+        else:
+            temp = self.start
+            while(temp.next != None):
+                temp = temp.next
+            temp.next = Node(value)
+    
+    def display(self):
+        if self.start == None:
+            print("LinkedList is Empty!")
+        else:
+            temp = self.start
+            while(temp != None):
+                print(temp.value, end="->")
+                temp = temp.next
+            print("End")
+    
+    # APPROACH 1, Count size, then go halfway
+    def size(self):
+        count = 0
+        if self.start == None:
+            print("LinkedList is Empty!")
+            return 0
+        else:
+            temp = self.start
+            while(temp != None):
+                count+=1
+                temp = temp.next
+            return count
+    
+    def Middle_1(self):
+        count = LL.size()
+        if count == 0:
+            print("No elements in list!")
+        if count%2 == 0:
+            counter = 0
+            temp = self.start
+            while(temp.next != None and counter != (count/2)-1):
+                counter+=1
+                temp = temp.next
+            print("Middle Value is",temp.value)
+            temp.next = temp.next.next
+        
+
+        if count%2 != 0:
+            counter = 0
+            temp = self.start
+            while(temp.next != None and counter != int(count/2)):
+                counter+=1
+                temp = temp.next
+            print("Middle Value is", temp.value)
+            temp.next = temp.next.next
+     
+
+LL = LinkedList()
+LL.display()
+LL.insert(10)
+LL.insert(20)
+LL.insert(30)
+LL.insert(40)
+LL.display()
+print(LL.size())
+LL.Middle_1()
