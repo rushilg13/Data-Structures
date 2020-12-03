6cd39f29119221c87dea7e116d21777adb2e9ff7
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-12-03 18:57:10 +0530

    Added method insert

--- Linked List/Linked List in Python/GFG.py
+++ Linked List/Linked List in Python/GFG.py
@@ -2,4 +2,16 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-        
+
+class LinkedList:
+    def __init__(self):
+        self.start = None
+
+    def insert(self, value):
+        if self.start == None:
+            self.start = Node(value)
+        else:
+            temp = self.start
+            while(temp != None):
+                temp = temp.next
+            temp.next = Node(value)
