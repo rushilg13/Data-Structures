0bf3fefaa63b95a0f730eab02f1abf3d92f30af7
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-12-04 02:45:24 +0530

    Added Problem

--- Linked List/Linked List in Python/GFG.py
+++ Linked List/Linked List in Python/GFG.py
@@ -188,30 +188,101 @@
 
 
 
-# https://www.careercup.com/question?id=5717797377146880
+# # https://www.careercup.com/question?id=5717797377146880
+
+# class Node:
+#     def __init__(self, value):
+#         self.value = value
+#         self.next = None
+
+# class Stack:
+#     def __init__(self):
+#         self.items = []
+    
+#     def push(self, item):
+#         self.items.append(item)
+    
+#     def pop(self):
+#         self.items.pop()
+    
+#     def sum_all(self):
+#         sum = 0
+#         for i in self.items:
+#             sum += i
+#         return sum
+#     def disp(self):
+#         return list(self.items)
+
+# class LinkedList:
+#     def __init__(self):
+#         self.start = None
+
+#     def insert(self, value):
+#         if self.start == None:
+#             self.start = Node(value)
+#         else:
+#             temp = self.start
+#             while(temp.next != None):
+#                 temp = temp.next
+#             temp.next = Node(value)
+    
+#     def display(self):
+#         if self.start == None:
+#             print("LinkedList is Empty!")
+#         else:
+#             temp = self.start
+#             while(temp != None):
+#                 print(temp.value, end="->")
+#                 temp = temp.next
+#             print("End")
+    
+#     def DeleteSumZeroNodes(self):
+#         if self.start == None:
+#             print("LinkedList Empty!")
+#             return
+#         else:
+#             temp = self.start
+#             stack = Stack()
+#             while(temp != None):
+#                 stack.push(temp.value)
+#                 temp = temp.next
+#                 if stack.sum_all() == 0:
+#                     stack = Stack()
+#             print(stack.disp())
+
+# LL = LinkedList()
+# LL.insert(6)
+# LL.insert(-6)
+# LL.insert(8)
+# LL.insert(4)
+# LL.insert(-12)
+# LL.insert(-8)
+# LL.insert(8)
+# LL.insert(-9)
+# LL.display()
+# LL.DeleteSumZeroNodes()
+                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# https://practice.geeksforgeeks.org/problem-page.php?pid=700196
 
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-
-class Stack:
-    def __init__(self):
-        self.items = []
-    
-    def push(self, item):
-        self.items.append(item)
-    
-    def pop(self):
-        self.items.pop()
-    
-    def sum_all(self):
-        sum = 0
-        for i in self.items:
-            sum += i
-        return sum
-    def disp(self):
-        return list(self.items)
 
 class LinkedList:
     def __init__(self):
@@ -225,41 +296,3 @@ class LinkedList:
             while(temp.next != None):
                 temp = temp.next
             temp.next = Node(value)
-    
-    def display(self):
-        if self.start == None:
-            print("LinkedList is Empty!")
-        else:
-            temp = self.start
-            while(temp != None):
-                print(temp.value, end="->")
-                temp = temp.next
-            print("End")
-    
-    def DeleteSumZeroNodes(self):
-        if self.start == None:
-            print("LinkedList Empty!")
-            return
-        else:
-            temp = self.start
-            stack = Stack()
-            while(temp != None):
-                stack.push(temp.value)
-                temp = temp.next
-                if stack.sum_all() == 0:
-                    stack = Stack()
-            print(stack.disp())
-
-LL = LinkedList()
-LL.insert(6)
-LL.insert(-6)
-LL.insert(8)
-LL.insert(4)
-LL.insert(-12)
-LL.insert(-8)
-LL.insert(8)
-LL.insert(-9)
-LL.display()
-LL.DeleteSumZeroNodes()
-                    
-
